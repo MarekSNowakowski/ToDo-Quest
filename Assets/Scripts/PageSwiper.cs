b887f48d096c37e1241d47daaa79ee992684e8a7
Marek Nowakowski <marsznow@gmail.com>
2020-11-19 22:08:35 +0100

    PageSwiper scroll fixed

--- Assets/Scripts/PageSwiper.cs
+++ Assets/Scripts/PageSwiper.cs
@@ -7,6 +7,7 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     private Vector3 panelLocation;
     private Vector3 indicatorLocation;
+    private Vector3 questsLocation;
     [SerializeField]
     float percentThreshold = 0.2f;
     [SerializeField]
@@ -15,26 +16,33 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
     int totalPages = 1;
     [SerializeField]
     GameObject panelIndicator;
-    public int currentPage = 1;
+    [SerializeField]
+    GameObject quests;
+    RectTransform questRT;
 
-    // Start is called before the first frame update
+    public int currentPage = 1;
 
     void Start()
     {
         panelLocation = transform.position;
         indicatorLocation = panelIndicator.transform.position;
+        questsLocation = quests.transform.position;
+        questRT = quests.GetComponent<RectTransform>();
     }
 
     public void OnDrag(PointerEventData data)
     {
-        float difference = data.pressPosition.x - data.position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
-        panelIndicator.transform.position = indicatorLocation + new Vector3(difference / totalPages, 0, 0);
+        Vector2 difference = data.pressPosition - data.position;
+        if(currentPage==1)
+        {
+            quests.transform.position = questsLocation - new Vector3(0, difference.y, 0);
+        }
     }
 
     public void OnEndDrag(PointerEventData data)
     {
-        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
+        Vector2 difference = data.pressPosition - data.position;
+        float percentage = difference.x / Screen.width;
         if (Mathf.Abs(percentage) >= percentThreshold)
         {
             Vector3 newLocation = panelLocation;
@@ -51,13 +59,28 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
                 newLocation += new Vector3(Screen.width, 0, 0);
                 newIndicatorLocation += new Vector3(-Screen.width / totalPages, 0, 0);
             }
-            StartCoroutine(SmoothMove(transform.position, newLocation, panelIndicator.transform.position, newIndicatorLocation, easing));
             panelLocation = newLocation;
             indicatorLocation = newIndicatorLocation;
-        }
-        else
-        {
+            StopAllCoroutines();
             StartCoroutine(SmoothMove(transform.position, panelLocation, panelIndicator.transform.position, indicatorLocation, easing));
+        }
+        else if (currentPage == 1) 
+        {
+            Debug.Log(quests.transform.position);
+            if(quests.transform.position.y < -175)
+            {
+                questsLocation.y = -175;
+                StartCoroutine(SmoothMoveScroll(quests.transform.position, questsLocation, easing));
+            }
+            else if(quests.transform.position.y > questRT.rect.height / 2)
+            {
+                questsLocation.y = questRT.rect.height / 2;
+                StartCoroutine(SmoothMoveScroll(quests.transform.position, questsLocation, easing));
+            }
+            else
+            {
+                questsLocation.y = quests.transform.position.y;
+            }
         }
     }
 
@@ -85,6 +108,20 @@ public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
             t += Time.deltaTime / seconds;
             transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
             panelIndicator.transform.position = Vector3.Lerp(indStartPos, indEndPos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+        quests.transform.position = new Vector3(quests.transform.position.x, questsLocation.y);
+    }
+
+    IEnumerator SmoothMoveScroll(Vector3 startpos, Vector3 endpos, float seconds)
+    {
+        startpos.x = Screen.width / 2;
+        endpos.x = Screen.width / 2;
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            quests.transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
             yield return null;
         }
     }
