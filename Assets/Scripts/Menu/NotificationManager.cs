72ff009b41e964fe2f8f5b6b6f6c4892388ecade
mareq022 <mareq022@gmail.com>
2021-03-21 18:29:25 +0100

    visiting app reminder WIP

--- Assets/Scripts/Menu/NotificationManager.cs
+++ Assets/Scripts/Menu/NotificationManager.cs
@@ -6,19 +6,20 @@ public class NotificationManager : MonoBehaviour
 {
     [SerializeField]
     SettingsManager settings;
-
-    private int notificationHour = 9;
+    private int remindNotificationID = 0;
 
     private void Start()
     {
         var channel = new AndroidNotificationChannel()
         {
-            Id = "channel_deadline",
-            Name = "Deadline Channel",
+            Id = "channel_todoquest",
+            Name = "ToDo Quest Channel",
             Importance = Importance.Default,
-            Description = "Deadline notifications",
+            Description = "ToDo Quest notifications",
         };
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
+        CancelNotification(remindNotificationID);
+        ShedudleRemindNotification(remindNotificationID);
     }
 
     public void ShedudleNotification(string title, DateTime notificationDay, int notificationID)
@@ -30,7 +31,19 @@ public class NotificationManager : MonoBehaviour
         notification.SmallIcon = "app_icon_small";
         notification.LargeIcon = "app_icon_large";
 
-        AndroidNotificationCenter.SendNotificationWithExplicitID(notification, "channel_deadline", notificationID);
+        AndroidNotificationCenter.SendNotificationWithExplicitID(notification, "channel_todoquest", notificationID);
+    }
+
+    public void ShedudleRemindNotification(int notificationID)
+    {
+        var notification = new AndroidNotification();
+        notification.Title = "We miss you!";
+        notification.Text = "You haven't visited our app in 3 days";
+        notification.FireTime = DateTime.Today.AddDays(3).AddHours(settings.GetNotificationHour()).AddMinutes(settings.GetNotificationMinutes());
+        notification.SmallIcon = "app_icon_small";
+        notification.LargeIcon = "app_icon_large";
+
+        AndroidNotificationCenter.SendNotificationWithExplicitID(notification, "channel_todoquest", notificationID);
     }
 
     public void CancelNotification(int notifactionID)
