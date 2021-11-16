5a16491d9a2db9da372794be27dcf7a125589b08
Marek Nowakowski <marsznow@gmail.com>
2021-11-17 00:03:24 +0100

    Overkeyboard removed

--- Assets/Scripts/AddPanel/AddPanelView.cs
+++ Assets/Scripts/AddPanel/AddPanelView.cs
@@ -48,31 +48,12 @@ public class AddPanelView : MonoBehaviour
     {
         nameInput.ActivateInputField();
         nameInput.Select();
-        StartCoroutine(WaitForKeyboardToOpen());
     }
 
     void OnEnable()
     {
         nameInput.ActivateInputField();
         nameInput.Select();
-        CheckIfKeyboardOpened();
-    }
-
-    IEnumerator WaitForKeyboardToOpen()
-    {
-        float waitingTime = 0.5f;
-        yield return new WaitForSeconds(waitingTime);
-        keyboardHeight = GetKeyboardHeight();
-        addPanel.anchoredPosition = new Vector2(0, keyboardHeight);
-        keyboardPlaceholder.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, keyboardHeight + (0.046f * Screen.height));
-    }
-
-    public void CheckIfKeyboardOpened()
-    {
-        if (keyboardHeight < 200)
-        {
-            StartCoroutine(WaitForKeyboardToOpen());
-        }
     }
 
     public void Close()
@@ -96,47 +77,6 @@ public class AddPanelView : MonoBehaviour
     public void CancelClose()
     {
         discardPaenl.SetActive(false);
-    }
-
-
-    /// <summary>
-    /// Returns the keyboard height in display pixels.
-    /// </summary>
-    public int GetKeyboardHeight()
-    {
-#if UNITY_EDITOR
-        return 685;
-#endif
-#if UNITY_ANDROID
-        using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-        {
-            var unityPlayer = unityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
-            var view = unityPlayer.Call<AndroidJavaObject>("getView");
-            var dialog = unityPlayer.Get<AndroidJavaObject>("mSoftInputDialog");
-
-            if (view == null || dialog == null)
-                return 0;
-
-            var decorHeight = 0;
-
-            /*if (includeInput)
-            {
-                var decorView = dialog.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView");
-
-                if (decorView != null)
-                    decorHeight = decorView.Call<int>("getHeight");
-            }*/
-
-            using (var rect = new AndroidJavaObject("android.graphics.Rect"))
-            {
-                view.Call("getWindowVisibleDisplayFrame", rect);
-                return (int)screenHeight - rect.Call<int>("height") + decorHeight;
-            }
-        }
-#else
-        var height = Mathf.RoundToInt(TouchScreenKeyboard.area.height);
-        return height >= Display.main.systemHeight ? 0 : height;
-#endif
     }
 
     public void selectRewardInputField()
--- Assets/Scripts/AddPanel/SubQuestAddView.cs
+++ Assets/Scripts/AddPanel/SubQuestAddView.cs
@@ -23,65 +23,11 @@ public class SubQuestAddView : MonoBehaviour
         myRectTransform = GetComponent<RectTransform>();
         nameInputField.ActivateInputField();
         nameInputField.Select();
-        StartCoroutine(WaitForKeyboardToOpen());
     }
 
     private void OnDisable()
     {
         nameInputField.text = "";
-    }
-
-    IEnumerator WaitForKeyboardToOpen()
-    {
-        float waitingTime = 0.5f;
-        yield return new WaitForSeconds(waitingTime);
-        keyboardHeight = GetKeyboardHeight();
-        myRectTransform.anchoredPosition = new Vector2(0, keyboardHeight);
-    }
-
-    public void CheckIfKeyboardOpened()
-    {
-        if (keyboardHeight < 200)
-        {
-            StartCoroutine(WaitForKeyboardToOpen());
-        }
-    }
-
-    public int GetKeyboardHeight()
-    {
-#if UNITY_EDITOR
-        return 685;
-#endif
-#if UNITY_ANDROID
-        using (var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-        {
-            var unityPlayer = unityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer");
-            var view = unityPlayer.Call<AndroidJavaObject>("getView");
-            var dialog = unityPlayer.Get<AndroidJavaObject>("mSoftInputDialog");
-
-            if (view == null || dialog == null)
-                return 0;
-
-            var decorHeight = 0;
-
-            /*if (includeInput)
-            {
-                var decorView = dialog.Call<AndroidJavaObject>("getWindow").Call<AndroidJavaObject>("getDecorView");
-
-                if (decorView != null)
-                    decorHeight = decorView.Call<int>("getHeight");
-            }*/
-
-            using (var rect = new AndroidJavaObject("android.graphics.Rect"))
-            {
-                view.Call("getWindowVisibleDisplayFrame", rect);
-                return (int)screenHeight - rect.Call<int>("height") + decorHeight;
-            }
-        }
-#else
-        var height = Mathf.RoundToInt(TouchScreenKeyboard.area.height);
-        return height >= Display.main.systemHeight ? 0 : height;
-#endif
     }
 
     public void AddSubQuest()
